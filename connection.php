4afee2897242780b1d7bf3c07065219d1ccb5737
Telérico Jones <snhr.jj@gmail.com>
2022-01-13 16:23:04 -0300

    Ordenação de resultados via PHP (melhor performance).

--- connection.php
+++ connection.php
@@ -10,52 +10,92 @@ function appendAND($sql, $bindings) {
     return $sql;
 }
 
-if (isset($_POST['submit'])) {
-    /* echo "<pre>"; */
-    /* var_dump($_POST); */
-    /* echo "</pre>"; */
-    /* die(); */
+/* echo "<pre>"; */
+/* var_dump($_POST); */
+/* echo "</pre>"; */
+/* die(); */
 
-    $search_param = "";
-    $search_type = "";
-    // Resultados da busca
-    $result = []; 
+$search_param = "";
+$search_type = "";
+$order_by = "Municipio";
+$order_by_direction = "ASC";
+$order_by_direction_invertion = "desc";
 
-    $bindings = [];
-    $sql = 'SELECT * FROM `provedores` WHERE ';
+// Resultados da busca
+$result = []; 
+$formInputs = [];
 
-    if (strlen($_POST["uf"]) > 0) {
-        $sql = appendAND($sql, $bindings);
-        $sql .= "UF_SEDE LIKE :uf ";
-        $bindings[] = [":uf", $_POST["uf"]];
-    }
+$bindings = [];
+$sql = 'SELECT * FROM `provedores` WHERE ';
 
-    if (strlen($_POST["cnpj"]) > 0) {
-        $sql = appendAND($sql, $bindings);
-        $sql .= "CNPJCPF LIKE :cnpj ";
-        $bindings[] = [":cnpj", $_POST["cnpj"]];
-    }
-
-    if (strlen($_POST["cidade"]) > 0) {
-        $sql = appendAND($sql, $bindings);
-        $sql .= "Municipio LIKE :cidade ";
-        $bindings[] = [":cidade", $_POST["cidade"]];
-    }
-
-    $sql .= '
-        ORDER BY Municipio ASC
-    ';
-
-    $stmt = $conn->prepare($sql);
-
-    for ($i = 0; $i < count($bindings); $i++) {
-        $stmt->bindParam($bindings[$i][0], $bindings[$i][1]);
-    }
-
-    $stmt->execute();
-
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    require __DIR__ . '/results.php';
-    /* require __DIR__ . '/result_provider.php'; */
+if (isset($_POST["uf"]) && strlen($_POST["uf"]) > 0) {
+    $sql = appendAND($sql, $bindings);
+    $sql .= "UF_SEDE LIKE :uf ";
+    $bindings[] = [":uf", $_POST["uf"]];
+    $formInputs[] = "<input type='hidden' name='uf' value='$_POST[uf]' />";
 }
+
+if (isset($_POST["cnpj"]) && strlen($_POST["cnpj"]) > 0) {
+    $sql = appendAND($sql, $bindings);
+    $sql .= "CNPJCPF LIKE :cnpj ";
+    $bindings[] = [":cnpj", $_POST["cnpj"]];
+    $formInputs[] = "<input type='hidden' name='cnpj' value='$_POST[cnpj]' />";
+}
+
+if (isset($_POST["cidade"]) && strlen($_POST["cidade"]) > 0) {
+    $sql = appendAND($sql, $bindings);
+    $sql .= "Municipio LIKE :cidade ";
+    $bindings[] = [":cidade", $_POST["cidade"]];
+    $formInputs[] = "<input type='hidden' name='cidade' value='$_POST[cidade]' />";
+
+}
+
+if (isset($_POST["regiao"]) && strlen($_POST["regiao"]) > 0) {
+    $sql = appendAND($sql, $bindings);
+    $sql .= "Regiao LIKE :regiao ";
+    $bindings[] = [":regiao", $_POST["regiao"]];
+    $formInputs[] = "<input type='hidden' name='regiao' value='$_POST[regiao]' />";
+}
+
+switch ($_POST["order_by_direction"]) {
+    case "asc":
+        $order_by_direction = "ASC";
+        $order_by_direction_invertion = "desc";
+        break;
+    case "desc":
+        $order_by_direction = "DESC";
+        $order_by_direction_invertion = "asc";
+        break;
+}
+
+switch ($_POST["order_by"]) {
+    case "nome":
+        $order_by = "Nome_Fantasia";
+        break;
+    case "uf":
+        $order_by = "UF_SEDE";
+        break;
+    case "cidade":
+        $order_by = "Municipio $order_by_direction, UF_SEDE";
+        break;
+    case "telefone":
+        $order_by = "Telefone_Principal";
+        break;
+}
+
+$sql .= "
+    ORDER BY $order_by $order_by_direction
+";
+
+$stmt = $conn->prepare($sql);
+
+for ($i = 0; $i < count($bindings); $i++) {
+    $stmt->bindParam($bindings[$i][0], $bindings[$i][1]);
+}
+
+$stmt->execute();
+
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+require __DIR__ . '/results.php';
+/* require __DIR__ . '/result_provider.php'; */
--- results.php
+++ results.php
@@ -1,3 +1,8 @@
+<?php
+function formatTelephone($tel) {
+    return preg_replace("/[^0-9]/", "", $tel);
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -37,9 +42,66 @@
                 <table class="table bg-light provedores" style="overflow: hidden">
                     <thead>
                         <tr>
-                            <th scope="col" style="width: 50%;">Nome Fantasia</th>
-                            <th scope="col" style="width: 30%;" order="asc">Municipio</th>
-                            <th scope="col" style="width: 20%;">UF</th>
+                            <th
+                                scope="col"
+                                style="width: 50%;"
+                                <?php
+                                    if ($_POST["order_by"] == "nome") {
+                                        echo "order='$order_by_direction_invertion'";
+                                    }
+                                ?>
+                            >
+                                <form action="/connection.php" method="post">
+                                    <?php echo join(" ", $formInputs) ?>
+                                    <input type="hidden" name="order_by" value="nome" />
+                                    <input
+                                        type="hidden"
+                                        name="order_by_direction"
+                                        value="<?php echo $order_by_direction_invertion ?>"
+                                    />
+                                </form>
+                                Nome Fantasia
+                            </th>
+                            <th
+                                scope="col"
+                                style="width: 30%;"
+                                <?php
+                                    if ($_POST["order_by"] == "cidade") {
+                                        echo "order='$order_by_direction_invertion'";
+                                    }
+                                ?>
+                            >
+                                <form action="/connection.php" method="post">
+                                    <?php echo join(" ", $formInputs) ?>
+                                    <input type="hidden" name="order_by" value="cidade" />
+                                    <input
+                                        type="hidden"
+                                        name="order_by_direction"
+                                        value="<?php echo $order_by_direction_invertion ?>"
+                                    />
+                                </form>
+                                Municipio / UF
+                            </th>
+                            <th
+                                scope="col"
+                                style="width: 20%;"
+                                <?php
+                                    if ($_POST["order_by"] == "telefone") {
+                                        echo "order='$order_by_direction_invertion'";
+                                    }
+                                ?>
+                            >
+                                <form action="/connection.php" method="post">
+                                    <?php echo join(" ", $formInputs) ?>
+                                    <input type="hidden" name="order_by" value="telefone" />
+                                    <input
+                                        type="hidden"
+                                        name="order_by_direction"
+                                        value="<?php echo $order_by_direction_invertion ?>"
+                                    />
+                                </form>
+                                Telefone
+                            </th>
                         </tr>
                     </thead>
                 </table>
@@ -58,15 +120,16 @@
                                 <td class="bg-light" style="width: 30%;">
                                 <form action="/result_provider.php" method="post" target="_blank">
                                     <input type="hidden" name="provider" value="<?php echo $row['id']; ?>" />
-                                    <button type="submit" class="normal btn btn-light btn-link"><?php echo $row['Municipio']?></button>
+                                    <button type="submit" class="normal btn btn-light btn-link"><?php echo $row['Municipio'] . ' / ' . $row['UF_Sede']?></button>
                                 </form>
                                 </td>
                                 <td class="separator"></td>
                                 <td class="bg-light" style="width: 20%;">
-                                <form action="/result_provider.php" method="post" target="_blank">
-                                    <input type="hidden" name="provider" value="<?php echo $row['id']; ?>" />
-                                    <button type="submit" class="normal btn btn-light btn-link"><?php echo $row['UF_Sede']?></button>
-                                </form>
+                                    <a href="tel:<?php
+                                        echo formatTelephone($row["Telefone_Principal"])
+                                    ?>">
+                                        <?php echo $row['Telefone_Principal']?>
+                                    </a>
                                 </td>
                             </tr>
                         <?php endforeach ?>
